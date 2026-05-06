8d91513971fabb7b9a7e289d3340bcee8d64c34c
rm-cg <guinto36ruth@gmail.com>
2026-05-07 04:14:40 +0800

    1 line

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,16 +1,1 @@
-// FIXED: We ONLY export the config types. 
-// We completely removed the missing content types so the linter stops looking for them!
-export type {
-  DisplayConfig,
-  RoutesConfig,
-  ProtectedRoutesConfig,
-  FontsConfig,
-  StyleConfig,
-  DataStyleConfig,
-  EffectsConfig,
-  MailchimpConfig,
-  SchemaConfig,
-  SameAsConfig,
-  SocialSharingConfig,
-  OnceUIConfig
-} from "./config.types";
+export * from "./config.types";
