00169eaa5ea6cc9bfb83cdc614a90f4808f36f88
rm-cg <guinto36ruth@gmail.com>
2026-05-09 13:47:06 +0800

    TRY

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import { Heading, Text, Button, Avatar, RevealFx, Column, Badge, Row, Schema, Li
 import { home, about, person, baseURL, routes } from "@/resources";
 import { Mailchimp } from "@/components";
 import { Projects } from "@/components/work/Projects";
-
 import { getPosts } from "@/utils/utils";
 import Post from "@/components/blog/Post";
+import ScrollablePosts from "@/components/blog/ScrollablePosts";
 
 export async function generateMetadata() {
   return {
@@ -35,7 +35,6 @@ export default function Home() {
         image={`/api/og/generate?title=${encodeURIComponent(home.title)}`}
         author={{ name: person.name, url: `${baseURL}${about.path}`, image: `${baseURL}${person.avatar}` }}
       />
-
       <Column fillWidth horizontal="center" gap="m">
         <Column maxWidth="s" horizontal="center" align="center">
           {home.featured.display && (
@@ -45,15 +44,12 @@ export default function Home() {
               </Badge>
             </RevealFx>
           )}
-
           <RevealFx translateY="4" fillWidth horizontal="center" paddingBottom="16">
             <Heading wrap="balance" variant="display-strong-l"> {home.headline} </Heading>
           </RevealFx>
-
           <RevealFx translateY="8" delay={0.2} fillWidth horizontal="center" paddingBottom="32">
             <Text wrap="balance" onBackground="neutral-weak" variant="heading-default-xl"> {home.subline} </Text>
           </RevealFx>
-
           <RevealFx paddingTop="12" delay={0.4} horizontal="center" paddingLeft="12">
             <Button id="about" data-border="rounded" href={about.path} variant="secondary" size="m" weight="default" arrowIcon>
               <Row gap="8" vertical="center" paddingRight="4">
@@ -64,11 +60,9 @@ export default function Home() {
           </RevealFx>
         </Column>
       </Column>
-
       <RevealFx translateY="16" delay={0.6}>
-        <Projects range={[1]} />
+        <Projects range={[7]} />
       </RevealFx>
-
       {routes["/blog"] && (
         <Column id="insights" fillWidth gap="24" marginBottom="l">
           <Row fillWidth paddingRight="64"> <Line maxWidth={48} /> </Row>
@@ -78,31 +72,20 @@ export default function Home() {
                 Professional Insights & Analytical Explorations
               </Heading>
             </Row>
-            
-            <Row 
-              flex={3} 
-              paddingX="20" 
-              overflowX="auto" 
-              gap="16" 
-              paddingBottom="24" 
-              style={{ scrollSnapType: "x mandatory", scrollbarWidth: "none" }}
-            >
+            <ScrollablePosts>
               {allPosts.map((post) => (
-                <Column 
-                  key={post.slug} 
-                  // FIXED: Moved the widths into standard CSS to bypass the strict linter!
+                <Column
+                  key={post.slug}
                   style={{ minWidth: "280px", maxWidth: "320px", scrollSnapAlign: "start" }}
                 >
                   <Post post={post} thumbnail direction="column" />
                 </Column>
               ))}
-            </Row>
-
+            </ScrollablePosts>
           </Row>
           <Row fillWidth paddingLeft="64" horizontal="end"> <Line maxWidth={48} /> </Row>
         </Column>
       )}
-
       <Mailchimp />
     </Column>
   );
